dcb90aaa435ddbad3162ce619b257ed81526a4a1
Carl Egonu <egonuc@uni.coventry.ac.uk>
2020-04-18 11:02:57 +0100

    working version

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
-import { StyleSheet,View} from 'react-native';
+import { StyleSheet,View,Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Footer from './components/Footer/BottomButtons';
 import Header from './components/header/header';
 import MuscleButton from './components/MuscleButton/MuscleButton';
 import Time from './components/Time/Time';
+
+const Stack = createStackNavigator();
 
 export default class App extends Component {
 
@@ -32,7 +36,7 @@ export default class App extends Component {
 
   homeSelected = () =>{
     if(this.state.homeSelected){
-      console.log('Home already selected')
+      console.log('Home already selected');
     }else{
       this.setState({
         homeSelected: true,
@@ -44,7 +48,7 @@ export default class App extends Component {
 
   muscleSelected = () =>{
     if(this.state.muscleSelected){
-      console.log('Muscle already selected')
+      console.log('Muscle already selected');
     }else{
       this.setState({
         homeSelected: false,
@@ -66,32 +70,46 @@ export default class App extends Component {
     }
   }
 
-  render(){
-    return (
+  Home=() =>{
+    return(
       <View style={styles.container}>
         <Header
-        homeSelected={this.state.homeSelected} 
-        goHome={this.homeSelected}/>
-
+          homeSelected={this.state.homeSelected} 
+          goHome={this.homeSelected}/>
         <View style={styles.time}>
           <Time/>
         </View>
-
         <View style={styles.muscleButtonView}>
           <MuscleButton
-          muscleSelected={this.muscleSelected}/>
+            muscleSelected={this.muscleSelected}/>
         </View>
-
         <View style={styles.navButtons}>
-          <Footer
-          homeTrue={this.state.homeSelected}
-          muscleTrue={this.state.muscleSelected} 
-          messageTrue={this.state.messageSelected}
-          homePressed={this.homeSelected}
-          musclePressed={this.muscleSelected}
-          messagePressed={this.messageSelected}/>
-        </View>
+            <Footer
+            homeTrue={this.state.homeSelected}
+            muscleTrue={this.state.muscleSelected} 
+            messageTrue={this.state.messageSelected}
+            homePressed={this.homeSelected}
+            musclePressed={this.muscleSelected}
+            messagePressed={this.messageSelected}/>
+          </View>
       </View>
+    )
+  }
+
+  render(){
+    return (
+      <NavigationContainer>
+        
+
+          <Stack.Navigator
+           initialRouteName="Home"
+           screenOptions={{
+             headerShown:false
+           }}>
+            <Stack.Screen name="Home" component={this.Home}/>
+          </Stack.Navigator>
+          
+      </NavigationContainer>
     );
   }
 }
